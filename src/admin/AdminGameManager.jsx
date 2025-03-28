b3aea5a6def32c6de15494d13a7d3d938dc48513
dukilee <gru.guilherme@gmail.com>
2025-03-28 14:54:04 -0300

    Sending toggle to server

--- src/admin/AdminGameManager.jsx
+++ src/admin/AdminGameManager.jsx
@@ -1,10 +1,38 @@
 import { io } from 'socket.io-client';
 import { useRef, useState, useEffect } from 'react';
-import Sidebar from './SideBar';
 import ToggleTable from './ToggleTable';
 import { QuestionType } from '../InputQuestionType';
 
 function AdminData(){
+  const socketRef = useRef(null);
+  const [number, updateNumber] = useState(19);
+  useEffect(() => {
+    console.log('Trying to connect to '+import.meta.env.VITE_SOCKET_URL+'...');
+    socketRef.current = io(import.meta.env.VITE_SOCKET_URL, {
+      transports: ['websocket'],
+    });
+
+    socketRef.current.on('connect', () => {
+      console.log('✅ Connected to server');
+    });
+
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, []);
+
+  const randomNumber = () => {
+    return Math.floor(Math.random()*30 - 10);
+  }
+
+  const generateNumberAndEmitData = () => {
+    const val = randomNumber();
+    updateNumber(val);
+    socketRef.current.emit('nextNumber', {
+      val, 'questionTypes': questionTypesActive.current
+    })
+  }
+
   const questionTypesAvailable = [
     QuestionType.ADD_TEN,
     QuestionType.ADD_TWENTY,
@@ -23,15 +51,18 @@ function AdminData(){
     }, {})
   );
 
-  console.log(questionTypesActive.current);
   const setQuestionType = (active, index) => {
     questionTypesActive.current[index] = active;
   }
 
   return <div>
-    <button onClick={() => {
-      console.log(questionTypesActive.current);
-    }} > VERIFY IT </button>
+    <div>
+      {number}
+      <button onClick={() => {
+        console.log(questionTypesActive.current);
+        generateNumberAndEmitData();
+      }} > VERIFY IT </button>
+    </div>
     <ToggleTable
       questionTypesAvailable={questionTypesAvailable}
       setQuestionType={setQuestionType}
